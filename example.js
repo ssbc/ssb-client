9d95a96980b0f866c90f8c49d5cd48bebdc355c0
Dominic Tarr <dominic.tarr@gmail.com>
2015-03-06 22:48:07 +1300

    add example

---
+++ example.js
@@ -0,0 +1,20 @@
+
+var Client = require('./')
+
+var config  = require('ssb-config')
+var ssbKeys = require('ssb-keys')
+var keys = ssbKeys.loadOrCreateSync(config)
+
+function abortIf (err) {
+  if(err) throw err
+}
+
+var client = Client(keys, config)
+  .connect(abortIf)
+  .auth(abortIf)
+
+client.whoami(function (err, whoami) {
+  abortIf(err)
+  console.log(whoami)
+  client.close()
+})
