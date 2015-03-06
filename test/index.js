618cfb143d0fc9dc508046d276fdeadb9b2963cf
Paul Frazee <pfrazee@gmail.com>
2015-03-06 14:18:35 -0600

    update test for api changes

--- test/index.js
+++ test/index.js
@@ -7,20 +7,21 @@ tape('test api', function (t) {
 
   var db = require('level-sublevel')(require('levelup')('/testdb', { db: require('memdown') }))
   var ssb = require('secure-scuttlebutt')(db, require('secure-scuttlebutt/defaults'))
-  var server = scuttlebot({ port: 45451, host: 'localhost' }, ssb, ssb.createFeed())
+  var server = scuttlebot({ port: 45451, host: 'localhost' }, ssb, ssb.createFeed()).use(require('scuttlebot/plugins/logging'))
 
   var keys = ssbkeys.generate()
-  var client = ssbclient(keys, {port: 45451, host: 'localhost'}, function (err, authed) {
+  var client = ssbclient(keys)
+  client.connect({ port: 45451, host: 'localhost' }, function (err) {
+    if (err)
+      throw err
+  })
+  client.publish({type: 'post', text: 'hello'}, function (err, data) {
     if(err) throw err
-    t.ok(authed.granted)
-    client.publish({type: 'post', text: 'hello'}, function (err, data) {
-      if(err) throw err
-      t.equal(data.value.content.text, 'hello')
-      console.log(data)
-      client.close(function() {
-        server.close()
-        t.end()
-      })
+    t.equal(data.value.content.text, 'hello')
+    console.log(data)
+    client.close(function() {
+      server.close()
+      t.end()
     })
   })
 })
