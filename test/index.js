638ef01c198d03f791ff29fc45457f41a2669129
Paul Frazee <pfrazee@gmail.com>
2015-03-10 19:46:58 -0500

    improve tests code

--- test/index.js
+++ test/index.js
@@ -3,30 +3,40 @@ var ssbkeys = require('ssb-keys')
 var tape = require('tape')
 var ssbclient = require('../index')
 
-tape('test api', function (t) {
-
-  var db = require('level-sublevel')(require('levelup')('/testdb', { db: require('memdown') }))
+function setupTest () {
+  var db = require('level-sublevel/bytewise')(require('levelup')('/testdb', { db: require('memdown'), keyEncoding: 'json', valueEncoding: 'json' }))
   var ssb = require('secure-scuttlebutt')(db, require('secure-scuttlebutt/defaults'))
   var server = scuttlebot({ port: 45451, host: 'localhost' }, ssb, ssb.createFeed()).use(require('scuttlebot/plugins/logging'))
 
   var keys = ssbkeys.generate()
-  var client = ssbclient(keys)
-  client.connect({ port: 45451, host: 'localhost' }, iferr)
+  var client = ssbclient({ port: 45451, host: 'localhost' })
+  client.connect(iferr)
   client.auth(ssbkeys.createAuth(keys), iferr)
 
-  var feed = client.createFeed(keys)
+  return {
+    server: server,
+    client: client,
+    keys: keys
+  }
+}
+
+function iferr (err) {
+  if (err)
+    throw err
+}
+
+tape('add messages', function (t) {
+
+  var env = setupTest()
+  var feed = env.client.createFeed(env.keys)
+
   feed.add({type: 'post', text: 'hello'}, function (err, data) {
     iferr(err)
     t.equal(data.value.content.text, 'hello')
     console.log(data)
-    client.close(function() {
-      server.close()
+    env.client.close(function() {
+      env.server.close()
       t.end()
     })
   })
-
-  function iferr (err) {
-    if (err)
-      throw err
-  }
 })
