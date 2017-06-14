a9d3ead1e16f2afc3700bcea863ba32e3399aaa4
Jan Bölsche <regular.gonzales@googlemail.com>
2017-06-14 13:02:24 +0200

    Add support for hash argument Does this use case actually makes sense?

--- blobs.js
+++ blobs.js
@@ -12,8 +12,12 @@ var multicb = require('multicb')
 // See ssb thread for details:
 // https://viewer.scuttlebot.io/%252YFBVzniDPuuyLnLk%2FsYSbIJzjhS7ctEIOv5frt9n9Q%3D.sha256
 
-module.exports = function(blobs_add) {
-  return function addBlobRaw(cb) {
+module.exports = function(blobs_add, blobs_rm) {
+  return function addBlobRaw(hash, cb) {
+    if ('function' === typeof hash) {
+      cb = hash
+      hash = undefined
+    }
     // taken from ssb-git-repo/lib/repo.js
     // work around sbot.blobs.add not calling back with blob id
     var done = multicb({ pluck: 1, spread: true })
@@ -21,7 +25,15 @@ module.exports = function(blobs_add) {
       ssbHash(done()),
       blobs_add(done())
     )
-    done(cb)
+    done(function(err, link) {
+      if (err || 'undefined' === typeof hash || hash == link) cb(err, link)
+      else {
+        blobs_rm(link, function(err) {
+          cb(new Error('Invalid blob hash value. expected: ' + hash + ', actual: ' + link))
+          if (err) console.error('Unable to blobs.rm blob with invalid hash', link, err)
+        })
+      }
+    })
     return sink
   }
 }
--- test/blobs.js
+++ test/blobs.js
@@ -28,7 +28,7 @@ test('if called with no hash, returns underlying sink and returns hash', functio
   )
 })
 
-test('if underlying sink errors, returns the rror', function(t) {
+test('if underlying sink errors, returns the error', function(t) {
 
   t.plan(2)
 
@@ -49,6 +49,83 @@ test('if underlying sink errors, returns the rror', function(t) {
     addBlob(function(err, link) {
       t.equal(err, error)
       t.notOk(link) 
+    })
+  )
+})
+
+test('if called with correct hash, behaves like called with no hash', function(t) {
+
+  t.plan(4)
+
+  function blobs_add_mock(cb) {
+    return pull.drain( function(buffer) {
+      t.equal(buffer, data)
+    }, function(err) {
+      t.equal(err, null)
+      cb(err);
+    })
+  }
+
+  var addBlob = decorate(blobs_add_mock)
+  pull(
+    pull.once(data),
+    addBlob(expectedLink, function(err, link) {
+      t.equal(err, null, 'should not error')
+      t.equal(link, expectedLink, 'should return expected link') 
+    })
+  )
+})
+
+test('if called with incorrect hash, errors and removes blob', function(t) {
+  t.plan(5)
+
+  function blobs_add_mock(cb) {
+    return pull.drain( function(buffer) {
+      t.equal(buffer, data, 'sink should receive correct data')
+    }, function(err) {
+      t.equal(err, null, 'sink should not receive an error')
+      cb(err);
+    })
+  }
+
+  function blobs_rm_mock(link, cb) {
+    t.equal(link, expectedLink, 'should try to remove correct blob')
+    cb(null)
+  }
+
+  var addBlob = decorate(blobs_add_mock, blobs_rm_mock)
+  pull(
+    pull.once(data),
+    addBlob("invalid link", function(err, link) {
+      t.assert(err, 'should error')
+      t.notOk(link, 'should not provide link') 
+    })
+  )
+})
+
+test('if called with incorrect hash and blob removal fails, ignore the error', function(t) {
+  t.plan(5)
+
+  function blobs_add_mock(cb) {
+    return pull.drain( function(buffer) {
+      t.equal(buffer, data, 'sink should receive correct data')
+    }, function(err) {
+      t.equal(err, null, 'sink should not receive an error')
+      cb(err);
+    })
+  }
+
+  function blobs_rm_mock(link, cb) {
+    t.equal(link, expectedLink, 'should try to remove correct blob')
+    cb(new Error('rm failed'))
+  }
+
+  var addBlob = decorate(blobs_add_mock, blobs_rm_mock)
+  pull(
+    pull.once(data),
+    addBlob("invalid link", function(err, link) {
+      t.assert(err, 'should error')
+      t.notOk(link, 'should not provide link') 
     })
   )
 })
